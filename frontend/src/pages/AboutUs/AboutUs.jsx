a5d1435332c334c745e10e5ab6dcef2fd4ea8e15
N4thh <nhatahh2003@gmail.com>
2025-05-14 11:42:41 +0900

    Update About Us Page
    Hoàn thiện gần xong about us
    (còn thiếu): image thành viên
    (đã thay thế): hình gà bằng hình mới đã cắt ảnh

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import './AboutUs.css'; 
+import ChickenHeader from '../../assets/images/1513628022992-DRNqAu2UEAAhIbj.png';
+import ChickkenInfo from '../../assets/images/269912a43432af5c9cf603093bb984c9.jpg';
+
 
 export default function AboutUs() {
   return (
-    <div>
-      <h2>Thông tin chung </h2>
+  <div>
+    <div className='About-Header'>
+          <h2 className="About-Title">ABOUT</h2>
+        <img src = {ChickenHeader} alt = 'Fried chicken dish at Friend & Chicken A' className='ChickenHeader' />
     </div>
+    <div className='StoreInfo'>
+      <div className='text'>
+        <h1>FRIEND & CHICKEN </h1>
+        <h3>Friend & Chicken was originally just a group project by the four of us. But with a shared passion for crispy fried chicken, we decided to turn it into a real startup. What began as an assignment quickly became our dream business — bringing delicious, golden chicken to hungry customers everywhere. 
+         With love, laughter, and a whole lot of oil, we proudly present ourselves as the first generation of a three-generation fried chicken empire.</h3>
+         <h2 style={{marginLeft:'150px'}}>Where the grease runs deep in the family.</h2>
+      </div>           
+          <img src = {ChickkenInfo} alt = '' className='image' /> 
+    </div>
+      <div className="image-section">
+        <div className="image-bar" />
+       <div className='rectangle' />
+      </div>
+      <div className='TeamInfo'>
+          <h2>Nguyen Thai Bao</h2>
+          <h2>Vu Viet Hung</h2>
+          <h2>Do Gia Bao</h2>
+          <h2>Le Dao Thai Tai</h2>
+          <h2>Nguyen Nhat Anh</h2>
+      </div>
+  </div>
   );
 }
