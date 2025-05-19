5583bb847641eaa56f8eebdb98499bcae3f069fd
N4thh <nhatahh2003@gmail.com>
2025-05-19 22:32:53 +0900

    Đổi phần app.js
    Thay đổi phần app.js đổi lại những phần sử dụng header - footer và những phần không

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import './assets/images/styles/main.css';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 
 import Home from "./pages/Home/Home";
 import Menu from "./pages/Menu/Menu";
@@ -20,34 +19,63 @@ import Account from "./pages/Account/Account";
 import Signup from "./pages/Signup/Signup";
 import Profile from "./pages/Profile/Profile";
 
+import AdminHome from "./pages/Admin/Home/Home";
+import AdminCustomer from "./pages/Admin/Customer/Customer";
+
+// Layout cho trang người dùng
+function ClientLayout() {
+  return (
+    <div className="container">
+      <Header />
+      <div className="content">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+
+function AdminLayout() {
+  return (
+    <div className="admin-container">
+      {/*Header -sidebar */}
+      <div className="admin-content">
+        <Outlet />
+      </div>
+      {/* footer*/}
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Header />
-        <div className="content">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/menu/chicken-rice" element={<MenuChickenRice />} />
-                <Route path="/menu/spaghetti" element={<MenuSpaghetti />} />
-                <Route path="/menu/combo" element={<MenuCombo />} />
-                <Route path="/menu/side-dishes" element={<MenuSideDishes />} />
-                <Route path="/news" element={<News />} />
-                <Route path="/restaurant" element={<Restaurant />} />
-                <Route path="/about-us" element={<AboutUs />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/account" element={<Account />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/profile" element={<Profile />} />
-                
-              </Routes>
-        </div>  
-        <Footer />       
-      </div>
-      
+      <Routes>
+    
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminHome />} />
+          <Route path="/admin/customer" element={<AdminCustomer />} />
+        </Route>
+        
+ 
+        <Route element={<ClientLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/menu/chicken-rice" element={<MenuChickenRice />} />
+          <Route path="/menu/spaghetti" element={<MenuSpaghetti />} />
+          <Route path="/menu/combo" element={<MenuCombo />} />
+          <Route path="/menu/side-dishes" element={<MenuSideDishes />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/restaurant" element={<Restaurant />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </Router>
-
   );
 }
 
---
+++ frontend/src/pages/Admin/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Home () {
+     return (
+            <h1>DashBroad</h1>
+     );
+}
+export default Home
